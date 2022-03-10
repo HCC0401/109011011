15d381903f2b1f562851de5a18d5bb4829f722dd
HCC0401 <x1998031890104@gmail.com>
2022-03-10 16:02:21 +0800

    add new file

---
+++ 109011011.py
@@ -0,0 +1,17 @@
+x= 5
+x= 10
+y= 2
+print(x)
+print(y)
+[remot "origin"]
+	url=https://github.com/HCC0401/109011011.git
+	fetch=+refs/heads/*:refs/remotes/origin/*
+[branch "main"]
+	remote = origin
+	merge = refs/heads/main
+[user]
+    git config = global user.email "x1998031890104@gmail.com"
+    git config = global user.name "HCC0401"
+[remote "origin"]
+	url = https://github.com/HCC0401/109011011.git
+	fetch = +refs/heads/*:refs/remotes/origin/*
